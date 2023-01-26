028af0d2f6c3952197fc1626a0b0b0b50b17fa3c
Facundo Fernandez <ff96_@hotmail.com>
2023-01-26 13:21:12 -0300

    add password encoder

--- src/main/java/com/reactjavacrudapp/demo/react/java/crud/app/security/PasswordEncoder.java
+++ src/main/java/com/reactjavacrudapp/demo/react/java/crud/app/security/PasswordEncoder.java
@@ -1,5 +1,15 @@
 package com.reactjavacrudapp.demo.react.java.crud.app.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
 public class PasswordEncoder {
+    
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
     
 }
--- src/main/java/com/reactjavacrudapp/demo/react/java/crud/app/security/config/WebSecurityConfig.java
+++ src/main/java/com/reactjavacrudapp/demo/react/java/crud/app/security/config/WebSecurityConfig.java
@@ -20,46 +20,48 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @AllArgsConstructor
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-  @Autowired
-  public RegistrationController signUpService;
-  
-  @Autowired
-  private final UserServiceImpl userServiceImpl;
+    @Autowired
+    public RegistrationController signUpService;
 
-  @Autowired;
-  public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-    auth.authenticationProvider();
-  }
-  
-  @Bean
-  public DaoAuthenticationProvider daoAuthenticationProvider(){
-      DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-      
-      provider.setPasswordEncoder();
-      provider.setUserDetailsService(userServiceImpl);
-      
-      return provider;
-  };
+    @Autowired
+    private final UserServiceImpl userServiceImpl;
+    
+    @Autowired
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(daoAuthenticationProvider());
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+
+        provider.setPasswordEncoder(bCryptPasswordEncoder);
+        provider.setUserDetailsService(userServiceImpl);
+
+        return provider;
+    }
 
   @Override
-  protected void configure(HttpSecurity http) throws Exception {
-    http
-            .authorizeRequests()
-            .antMatchers("/api/v*/registration/**") 
-            .permitAll()
-            .anyRequest()
-            .authenticated()
-            .and().formLogin()
-            .loginPage("/login")
-            .loginProcessingUrl("/logincheck")
-            .usernameParameter("email")
-            .passwordParameter("password")
-            .defaultSuccessUrl("/") //cambiar bien esto
-            .permitAll()
-            .and().logout()
-            .logoutUrl("/logout")
-            .logoutSuccessUrl("/")
-            .permitAll();
-  }
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/api/v*/registration/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and().formLogin()
+                .loginPage("/login")
+                .loginProcessingUrl("/logincheck")
+                .usernameParameter("email")
+                .passwordParameter("password")
+                .defaultSuccessUrl("/") //cambiar bien esto
+                .permitAll()
+                .and().logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/")
+                .permitAll();
+    }
 
 }
